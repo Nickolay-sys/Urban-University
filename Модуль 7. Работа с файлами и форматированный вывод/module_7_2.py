6f024eb60cd7efef98995311a4c7445913acee83
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-10 21:12:51 +0300

    Модуль 7

--- Модуль 7. Работа с файлами и форматированный вывод/module_7_2.py
+++ Модуль 7. Работа с файлами и форматированный вывод/module_7_2.py
@@ -1,12 +1,14 @@
 import io
 def custom_write(file_name, strings):
-    file= open(file_name, 'wt', encoding='utf-8')
-    file.writelines(strings)
+    file= open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i,j in enumerate(strings):
+        key = (i+1, file.tell())
+        strings_positions[key] = j
+        file.write(j + '\n')
     file.close
-    numbers = (file.seek(0), file.seek(16), file.seek(66), file.seek(98))
-    strings_positions = dict(zip(enumerate(numbers,+1), strings))
-    return strings_positions 
-  
+    return strings_positions
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
