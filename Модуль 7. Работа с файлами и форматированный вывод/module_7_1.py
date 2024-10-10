f6ac0db35e9e00c929cc743729c0004e25bf1098
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-10 18:11:31 +0300

    Модуль 7

--- Модуль 7. Работа с файлами и форматированный вывод/module_7_1.py
+++ Модуль 7. Работа с файлами и форматированный вывод/module_7_1.py
@@ -1,33 +1,66 @@
+# class Product:
+#     def __init__(self, name: str, weight: float, category: str):
+#         self.name = name
+#         self.weight = weight
+#         self.category = category
+#     def __str__(self):
+#         return f'{self.name}, {self.weight}, {self.category}'
+    
+# class Shop(Product):
+#     def __init__(self, name: str, weight: float, category: str, __file_name = 'products.txt'):
+#         super().__init__(name, weight, category)
+#         self.__file_name = __file_name
+#     def get_products(self):
+#         file = open(self.__file_name, 'r')    
+#         lst = file.read()
+#         file.close()
+#         print(f'{lst}')
+#     def add(self, *products):
+#         for i in products:
+#             prod = str(i)
+#             file = open(self.__file_name, 'r')    
+#             lst = file.read()
+#             file.close()
+#             if prod in lst:
+#                 print(f'Продукт {prod} уже есть в магазине')
+#             else:
+#                 file = open(self.__file_name,'a')
+#                 lst = file.write(f'\n{prod}')
+#                 file.close()
 class Product:
-    def __init__(self, name: str, weight: float, category: str):
+    def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
         self.category = category
-    def __str__(self):
+        
+    def __str__(self) -> str:
         return f'{self.name}, {self.weight}, {self.category}'
     
 class Shop(Product):
-    def __init__(self, name: str, weight: float, category: str, __file_name = 'products.txt'):
+    def __init__(self, name, weight, category):
         super().__init__(name, weight, category)
-        self.__file_name = __file_name
+        self.__file_name = 'products.txt'    
+    
     def get_products(self):
-        file = open(self.__file_name, 'r')    
-        lst = file.read()
+        file = open(self.__file_name, 'r')
+        print(file.read())
         file.close()
-        print(f'{lst}')
+        
     def add(self, *products):
+        file = open(self.__file_name, 'r')
+        list_1 = file.read()
+        file.close()
         for i in products:
             prod = str(i)
-            file = open(self.__file_name, 'r')    
-            lst = file.read()
-            file.close()
-            if prod in lst:
-                print(f'Продукт {prod} уже есть в магазине')
+            if prod in list_1:
+                print(f'Продукт {self.name} уже есть в магазине')
             else:
-                file = open(self.__file_name,'a')
-                lst = file.write(f'\n{prod}')
+                file = open(self.__file_name, 'a')
+                file.write(f'\n{prod}')
                 file.close()
 
+        
+                
 s1 = Shop('', '', '')
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
--- Модуль 7. Работа с файлами и форматированный вывод/module_7_2.py
+++ Модуль 7. Работа с файлами и форматированный вывод/module_7_2.py
@@ -1,20 +1,19 @@
+import io
 def custom_write(file_name, strings):
-    strings_positions = {}
-    file = open(file_name, 'a', encoding='utf-8')
-    for i in strings:
-        file.tell()
-        
-
-        
+    file= open(file_name, 'wt', encoding='utf-8')
+    file.writelines(strings)
+    file.close
+    numbers = (file.seek(0), file.seek(16), file.seek(66), file.seek(98))
+    strings_positions = dict(zip(enumerate(numbers,+1), strings))
+    return strings_positions 
+  
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+    ] 
 
-result = custom_write('test.txt', info)
+result = custom_write('test.txt',info)
 for elem in result.items():
   print(elem)
-
-
