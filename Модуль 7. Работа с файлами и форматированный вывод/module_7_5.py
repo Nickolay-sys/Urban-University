357eb956aec9ef6aee818dc3af473291aa98f02c
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-12 00:27:45 +0300

    Модуль 7

--- Модуль 7. Работа с файлами и форматированный вывод/module_7_5.py
+++ Модуль 7. Работа с файлами и форматированный вывод/module_7_5.py
@@ -11,10 +11,9 @@ os.path.dirname(directory)
 
 for root,dirs,files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
+        filepath = os.path.join(root, file)
         filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.stat(file).st_size
-        parent_dir = os.path.dirname(file)
-        
-print(f'Обнаружен файл: {file}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
