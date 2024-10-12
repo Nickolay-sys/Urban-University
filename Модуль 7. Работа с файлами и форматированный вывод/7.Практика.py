1e5487ff1c4f519b47d2756ae5d91b2371576c8d
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-12 16:05:42 +0300

    Модуль 7

--- Модуль 7. Работа с файлами и форматированный вывод/7.Практика.py
+++ Модуль 7. Работа с файлами и форматированный вывод/7.Практика.py
@@ -1,12 +1,22 @@
 import tkinter
+from tkinter import filedialog
+import os
 
+def file_select():
+    filename = filedialog.askopenfilename(initialdir='/',title='Выберите файл',
+                                          filetypes=(('Текстовый файл','.txt'),('Все файлы','*')))
+    text['text'] = text['text'] + filename
+    os.startfile(filename)
+    
 window = tkinter.Tk()
 window.title('Проводник')
 window.geometry('500x500')
-window.configure(bg='gold')
+window.configure(bg='grey')
 window.resizable(False, False)
-text = tkinter.Label(window, text='Файл', height=5 ,width=20, background='silver')
+text = tkinter.Label(window, text='Файл', height=5 ,width=71, 
+                     background='silver')
 text.grid(column=1,row=1)
-button_select = tkinter.Button(window, width=20, height=3, text='Выберите файл')
+button_select = tkinter.Button(window, width=20, height=3, 
+                               text='Выберите файл',command=file_select)
 button_select.grid(column=1, row=2)
 window.mainloop()
--- Модуль 7. Работа с файлами и форматированный вывод/module_7_5.py
+++ Модуль 7. Работа с файлами и форматированный вывод/module_7_5.py
@@ -12,7 +12,7 @@ os.path.dirname(directory)
 for root,dirs,files in os.walk(directory):
     for file in files:
         filepath = os.path.join(root, file)
-        filetime = os.path.getmtime(file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(filepath)
