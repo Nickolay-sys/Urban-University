901744d58ee03d3299e2d2d8487351bc9978bd20
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-19 21:00:40 +0300

    Д/З по теме "Генераторы"

--- Модуль 9. Инструменты функционального программирования/module_9_6/module_9_6.py
+++ Модуль 9. Инструменты функционального программирования/module_9_6/module_9_6.py
@@ -5,8 +5,7 @@ def all_variants(text):
         if lines != letters:
             yield lines + letters
     yield text
-              
-        
+                     
 a = all_variants("abc")
 for i in a:
     print(i)
