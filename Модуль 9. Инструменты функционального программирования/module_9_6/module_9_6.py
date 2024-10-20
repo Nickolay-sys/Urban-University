d1a773927f28387d2a0701dd68238aa9f9fef192
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2024-10-20 13:05:03 +0300

    Домашнее задание по теме "Генераторы"

--- Модуль 9. Инструменты функционального программирования/module_9_6/module_9_6.py
+++ Модуль 9. Инструменты функционального программирования/module_9_6/module_9_6.py
@@ -1,11 +1,9 @@
 def all_variants(text):
-    for letters in text:
-        yield letters
-    for lines in text:
-        if lines != letters:
-            yield lines + letters
-    yield text
-                     
+    for x in range(len(text)):
+        for y in range(len(text) - x):
+            yield text[y:x + y + 1]
+
+            
 a = all_variants("abc")
 for i in a:
     print(i)
