65258e6d781b580470d992d1bc082935d5eef2d2
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2025-01-13 15:09:58 +0300

    Д/З "Основы Fast Api и маршрутизация"

--- Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_1.py
+++ Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def read_root() -> dict:
-    return {'message':'Главная страница'}
+async def read_root() -> str:
+    return 'Главная страница'
 
 @app.get('/user/admin')
-async def read_adim() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def read_admin() -> str:
+    return "Вы вошли как администратор"
 
 @app.get('/user/{user_id}')
-async def read_user_id(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def read_user_id(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get('/user')
-async def read_user(username: str, age: int) -> dict:
-    return {"message": f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def read_user(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
