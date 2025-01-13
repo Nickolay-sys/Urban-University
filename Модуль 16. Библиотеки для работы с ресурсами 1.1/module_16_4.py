dca1aea986377450c47910c4ce3236e6d64aa0a8
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2025-01-13 17:05:09 +0300

    "Модели данных Pydantic"

--- Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_4.py
+++ Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_4.py
@@ -7,54 +7,53 @@ app = FastAPI()
 users = []
 
 class User(BaseModel):
-    id: int = None
+    id: int 
     username: str 
-    age: int = None
-
+    age: int 
+    
 @app.get('/users')
 async def get_users() -> List[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-async def user_registration(user: User,
-                            username: Annotated[str, Path(min_length=5,
-                                                          max_length=20,
-                                                          title='Enter username')],
-                            age: Annotated[int, Path(ge=18,
-                                                     le=120,
-                                                     title='Enter age')]):
-    if not users:
-        user.id = 1
-    else:
-        user.id = users[-1].id + 1
-    user.username = username
-    user.age = age
-    users.append(user)
-    return user
-        
+async def add_user(username: Annotated[str, Path(min_length= 5,
+                                                 max_length= 20,
+                                                 title= 'Enter username')
+                                       ],
+                   age: Annotated[int, Path(ge= 18,
+                                            le= 120,
+                                            title= 'Enter age')]) -> User:
+    user_id = 1 if not users else users[-1].id + 1
+    new_user = User(id= user_id, username= username, age= age)
+    users.append(new_user)
+    return new_user
+    
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: Annotated[int, Path(gt=0,
-                                                   le=100,
-                                                   title='Enter ID')],
-                      username: Annotated[str,Path(min_length=5,
-                                                   max_length=20,
-                                                   title='Enter username')],
-                      age: Annotated[int,Path(ge=18,
-                                              le=120,
-                                              title='Enter age')],
-                      user: str = Body()):
-    try:
-        user_edits = users[user_id - 1] 
-        user_edits.username = username
-        user_edits.age = age
-        return user_edits
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+async def update_user(user_id: Annotated[int, Path(gt= 0,
+                                                   le= 100,
+                                                   title= 'Enter ID')
+                                         ], 
+                      username: Annotated[str, Path(min_length= 5,
+                                                    max_length= 20,
+                                                    title= 'Enter new username')
+                                          ], 
+                      age: Annotated[int, Path(ge= 18,
+                                               le= 120,
+                                               title= 'Enter new age')
+                                     ]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age 
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int):
+async def del_user(user_id: Annotated[int, Path(gt= 0,
+                                                le= 100,
+                                                title= 'Enter ID for deletion')]):
     for user in users:
         if user.id == user_id:
             users.remove(user)
             return user
-    return HTTPException(status_code=404, detail='User was not found')
+    raise HTTPException(status_code=404, detail='User was not found')
