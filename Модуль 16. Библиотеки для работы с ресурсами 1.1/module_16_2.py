4c52595f793f5b7e83eb403f78030b6a24b6a22f
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2025-01-13 15:20:21 +0300

    Д/З "Валидация данных"

--- Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_2.py
+++ Модуль 16. Библиотеки для работы с ресурсами 1.1/module_16_2.py
@@ -4,28 +4,33 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get('/')
-async def read_root() -> dict:
-    return {'message':'Главная страница'}
+async def read_root() -> str:
+    return 'Главная страница'
 
 @app.get('/user/admin')
-async def read_adim() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def read_adim() -> str:
+    return "Вы вошли как администратор"
 
 @app.get('/user/{user_id}')
 async def read_user_id(user_id: Annotated[int, Path(gt=0,
                                                     le=100,
-                                                    title='Enter User ID')]
-                       ) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+                                                    title='Enter User ID',
+                                                    examples='1')
+                                          ]
+                       ) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get('/user/{username}/{age}')
 async def read_user(username: Annotated[str, 
                                         Path(min_length=5,
                                              max_length= 20,
-                                             title='Enter username') 
+                                             title='Enter username',
+                                             examples='Nikolay') 
                                         ], 
                     age: Annotated[int, Path(ge=18,
                                              le=120,
-                                             title="Enter age")]
-                    ) -> dict:
-    return {"message": f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+                                             title="Enter age",
+                                             examples='26')
+                                   ]
+                    ) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
