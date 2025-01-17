232ae30cd528068b1b6a5b700096f1c2fd0d55e4
«Nickolay-sys» <QuokkaNikolay@yandex.ru>
2025-01-17 17:57:07 +0300

    Использование БД в маршрутизации. 1.1

---
+++ Модуль 17. Библиотека для работы с ресурсами 2.1/module_17_4/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from backend.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
---
+++ Модуль 17. Библиотека для работы с ресурсами 2.1/module_17_4/app/routers/user.py
@@ -0,0 +1,70 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from backend.db_depends import get_db
+from models.user import User
+from typing import Annotated
+from schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify 
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+@router.get("/")
+async def all_user(db: Annotated[Session, Depends(get_db)]):
+    users = db.scalars(select(User)).all()
+    if users is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no users"
+        )
+    return users
+
+@router.get("/user_id")
+async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = db.scalars(select(User)).where(User.id == user_id)
+    if user is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User was not found"
+        )
+    return user
+    
+@router.post("/create")
+async def create_user(create: CreateUser, db: Annotated[Session, Depends(get_db)]):
+    db.execute(insert(User).values(
+        username=create.username,
+        firstname=create.firstname,
+        lastname=create.lastname,
+        age=create.age,
+        slug=slugify(create.username))
+    )
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
+@router.put("/update")
+async def update_user(user_update: UpdateUser, user_id: int, db: Annotated[Session, Depends(get_db)]):
+    update_user = db.scalars(select(User).where(User.id == user_id).first())
+    if update_user is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User was not found"
+        )
+    db.execute(update(User).where(User.id == user_id).values(
+        firstname=user_update.firstname,
+        lastname=user_update.lastname,
+        age=user_update.age
+    ))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
+@router.delete("/delete")
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    delete_user = db.scalars(select(User).where(User.id == user_id).first())
+    if delete_user is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User was not found"
+        )
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
